bc9621f278c2e461f652d0ada65cb51d7ffff5aa
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-12 21:21:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     :memorial_day => ["BBQ"]
   }
 }
-holiday_hash[:season][:holiday_name] = supply_array
+holiday_hash[season][holiday_name] = supply_array
 holiday_hash
 
 end
