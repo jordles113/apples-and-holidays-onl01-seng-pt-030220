910228604246f62c2fff66ecbcda9474dcf30f59
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-12 21:12:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   }
 }
 holiday_hash[:winter].each do |holiday, decorations|
-  decorations << supply
+  decorations.push supply
 end 
 
 end
