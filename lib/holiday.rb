1cdac64832e694e42809c3e876c1910ab3073485
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-12 21:23:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -133,7 +133,10 @@ def all_supplies_in_holidays(holiday_hash)
     :memorial_day => ["BBQ"]
   }
 }
-holiday_hash[]
+ holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
